49d4a1fe2a4448d809087c89552163b6a159288b
“Artyvas” <artemvasily@gmail.com>
2021-03-14 13:45:36 -0700

    Links frontend to backend via GET request from frontend to backend - creates index action for Key model

---
+++ src/views/Keys.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="home">
+<h1> All keys</h1>
+<div v-for="key in keys" v-bind:key="key.id">
+  <img v-bind:src="key.image_url" alt="" />
+  <h2>
+    Name: {{ key.name }}
+  </h2>
+</div>
+</div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      keys: [],
+    };
+  },
+    created: function() {
+      this.indexKeys();
+    },
+    methods: {
+      indexKeys: function() {
+        axios.get("/api/keys").then(response => {
+          this.keys = response.data;
+          console.log("All Keys:", this.keys);
+        });
+      },
+    }
+  }
+</script>
