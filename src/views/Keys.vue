b977491f42592be6ad1ececf76cb2aeeb516c124
“Artyvas” <artemvasily@gmail.com>
2021-03-15 17:52:27 -0700

    Installs Bootstrap theme for sidebar and top navbar - can start styling individual pages

--- src/views/Keys.vue
+++ src/views/Keys.vue
@@ -1,4 +1,5 @@
 <template>
+
 <div class="home">
 <h1> All keys</h1>
 
@@ -6,11 +7,13 @@ Search by Key name: <input v-model="nameFilter" type="text" list="key-names">
 <datalist id="key-names">
   <option v-for="key in keys" v-bind:key="key.id"> {{ key.name }} </option>
 </datalist>
+<div is="transition-group" class="row" appear enter-active-class="animated fadeIn" leave-animated-class="animated fadeOut">
 <div v-for="key in filterBy(keys, nameFilter, 'name')" v-bind:key="key.id">
   <img v-bind:src="key.image_url" alt="" />
   <h2>
-    Name: {{ key.name }}
+    {{ key.name }}
   </h2>
+  </div>
 </div>
 </div>
 </template>
